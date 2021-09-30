5b1735d326d4a3c9057873511530a0b9ef53b4f5
Vahideh Ghanbari <vahideh.gh@gmail.com>
2021-09-29 21:12:24 -0700

    HW3-Week2

---
+++ module.py
@@ -0,0 +1,9 @@
+def square_feet(l,w):
+    area = l * w
+    
+    print(f"The house is {area} square feet.")
+    
+    
+def circle_circum(r):
+    c = 2 * 3.14 * r
+    print(f"The circumference is {c}")
